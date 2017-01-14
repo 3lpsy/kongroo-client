c98256e06183d143337e06f15644c083beadba6c
apostle <apostle@apostles-MacBook-Pro.local>
2017-01-13 18:56:26 -0600

    pages

---
+++ src/pages/article/index/IndexHero.vue
@@ -0,0 +1,54 @@
+<template lang="html">
+    <section class="hero is-primary">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <h1 class="title">
+                    Articles
+                </h1>
+                <h2 class="subtitle">
+                </h2>
+            </div>
+        </div>
+        <div class="hero-foot">
+            <div class="container has-text-centered">
+                <nav class="tabs is-boxed is-fullwidth">
+                    <ul>
+                        <li class="" v-for="tag in menuTags" v-if="tag.id">
+                            <app-link
+                                class="ArticlePreview__tag-link Link"
+                                r-route="article.index"
+                                :r-params="{}"
+                                :r-query="{'tags[]': [tag.id]}"
+                                :query-only="true">
+                                <span slot="link">
+                                    {{tag.name}}
+                                </span>
+                            </app-link>
+                        </li>
+                    </ul>
+                  </nav>
+            </div>
+        </div>
+    </section>
+
+</template>
+
+<script>
+import loader from "../../../utils/loader";
+export default {
+    computed: {
+        tags() {
+            return this.$store.getters['tag/getters/tagRepository'];
+        },
+        menuTags() {
+            return this.tags.slice(0, 5);
+        }
+    },
+    components: {
+        AppLink: loader.link(),
+    }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/pages/article/index/IndexMain.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+
+    <div class="container">
+        <div class="columns">
+            <div class="column is-two-thirds-desktop">
+                <article-preview-list>
+                </article-preview-list>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+import loader from "../../../utils/loader";
+
+export default {
+    components: {
+        ArticlePreviewList: loader.component("article", "article-preview-list"),
+    }
+}
+</script>
+
+<style lang="css">
+</style>
